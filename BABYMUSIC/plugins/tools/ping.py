eab1b92148adcd3eac735ede57d8342ace2d8eaf
Yasir Akhtar <yasiralam662@gmail.com>
2025-01-02 03:59:37 +0530

    New Btn !

--- BABYMUSIC/plugins/tools/ping.py
+++ BABYMUSIC/plugins/tools/ping.py
@@ -11,13 +11,13 @@ from BABYMUSIC.utils.inline import supp_markup
 from config import BANNED_USERS
 
 
-@app.on_message(filters.command("ping", prefixes=["/"]) & ~BANNED_USERS)
+@app.on_message(filters.command("ping", prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & ~BANNED_USERS)
 @language
 async def ping_com(client, message: Message, _):
     start = datetime.now()
-    # Changed from reply_video to reply
-    response = await message.reply(
-        text=_["ping_1"].format(app.mention),
+    response = await message.reply_video(
+        video="https://graph.org/file/5690109178f081adf464d.mp4",
+        caption=_["ping_1"].format(app.mention),
     )
     pytgping = await BABY.ping()
     UP, CPU, RAM, DISK = await bot_sys_stats()
