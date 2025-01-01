eb650cae1ec5e9b3bd597565922cfbb95b91814e
Yasir Akhtar <yasiralam662@gmail.com>
2025-01-02 01:57:10 +0530

    New !

--- BABYMUSIC/utils/inline/start.py
+++ BABYMUSIC/utils/inline/start.py
@@ -28,7 +28,7 @@ def private_panel(_):
             )
         ],
         [
-            InlineKeyboardButton(text=_["D_B_1"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
