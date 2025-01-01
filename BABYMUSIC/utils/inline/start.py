4372e41cfbd5ab290d0bd2a54c0fe4c216191b6b
Yasir Akhtar <yasiralam662@gmail.com>
2025-01-02 01:47:38 +0530

    new update

--- BABYMUSIC/utils/inline/start.py
+++ BABYMUSIC/utils/inline/start.py
@@ -27,14 +27,10 @@ def private_panel(_):
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
         ],
-        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text=_["D_B_1"], url=config.SUPPORT_CHANNEL),
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
-        [
-            InlineKeyboardButton(text=_["S_B_5"], url=config.CHAT),
-            InlineKeyboardButton(text=_["S_B_7"], url=config.SOURCE),
-        ],
+        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
     ]
     return buttons
