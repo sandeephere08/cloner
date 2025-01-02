3e0695f9386d0ba5f689eb2524fe0750354b329a
Yasir Akhtar <yasiralam662@gmail.com>
2025-01-03 02:37:06 +0530

    New CLone !

--- BABYMUSIC/utils/inline/help.py
+++ BABYMUSIC/utils/inline/help.py
@@ -57,3 +57,59 @@ def private_help_panel(_):
         ],
     ]
     return buttons
+
+
+def first_page(_):
+    controll_button = [
+        InlineKeyboardButton(text="๏ ᴍᴇɴᴜ ๏", callback_data=f"settingsback_helper"),
+        InlineKeyboardButton(text="๏ ɴᴇxᴛ ๏", callback_data=f"dilXaditi"),
+    ]
+    first_page_menu = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    text=_["H_B_1"], callback_data="help_callback hb1"
+                ),
+                InlineKeyboardButton(
+                    text=_["H_B_2"], callback_data="help_callback hb2"
+                ),
+                InlineKeyboardButton(
+                    text=_["H_B_3"], callback_data="help_callback hb3"
+                ),
+            ],
+            [
+                InlineKeyboardButton(
+                    text=_["H_B_4"], callback_data="help_callback hb4"
+                ),
+                InlineKeyboardButton(
+                    text=_["H_B_12"], callback_data="help_callback hb12"
+                ),
+                InlineKeyboardButton(
+                    text=_["H_B_5"], callback_data="help_callback hb5"
+                ),
+            ],
+            [
+                InlineKeyboardButton(
+                    text=_["H_B_6"], callback_data="help_callback hb6"
+                ),
+                InlineKeyboardButton(
+                    text=_["H_B_10"], callback_data="help_callback hb10"
+                ),
+            ],
+            [
+                InlineKeyboardButton(
+                    text=_["H_B_8"], callback_data="help_callback hb8"
+                ),
+                InlineKeyboardButton(
+                    text=_["H_B_9"], callback_data="help_callback hb9"
+                ),
+            ],
+            [
+                InlineKeyboardButton(
+                    text=_["H_B_11"], callback_data="help_callback hb11"
+                )
+            ],
+            controll_button,
+        ]
+    )
+    return first_page_menu
