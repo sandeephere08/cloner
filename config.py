bbe798024e380aca985a664c7ce238292bfd1725
Yasir Akhtar <yasiralam662@gmail.com>
2025-01-02 14:45:00 +0530

    New !

--- config.py
+++ config.py
@@ -54,7 +54,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/AimmXD/SIRIMUSIC",
+    "https://github.com/ProPrivates/SIRIMUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
