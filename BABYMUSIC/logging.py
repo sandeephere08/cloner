44143ee38717eff2ded89cccde135d5af7ab73d6
Yasir Akhtar <yasiralam662@gmail.com>
2025-01-03 01:51:51 +0530

    New !

--- BABYMUSIC/logging.py
+++ BABYMUSIC/logging.py
@@ -1,14 +1,5 @@
 import logging
 
-# Create a custom filter to suppress pyrogram-specific errors
-class NoPyrogramErrorFilter(logging.Filter):
-    def filter(self, record):
-        # Suppress any messages from pyrogram
-        if "Peer id invalid" in record.getMessage():
-            return False
-        return True
-
-# Set up the basic logging configuration
 logging.basicConfig(
     level=logging.INFO,
     format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
@@ -19,11 +10,10 @@ logging.basicConfig(
     ],
 )
 
-# Add the custom filter to suppress the "Peer id invalid" messages
-logging.getLogger("pyrogram").addFilter(NoPyrogramErrorFilter())
-logging.getLogger("httpx").setLevel(logging.ERROR)
-logging.getLogger("pytgcalls").setLevel(logging.ERROR)
+# logging.getLogger("httpx").setLevel(logging.ERROR)
+# logging.getLogger("pyrogram").setLevel(logging.ERROR)
+# logging.getLogger("pytgcalls").setLevel(logging.ERROR)
 
-# Function to get a logger
-def LOGGER(name: str) -> logging.Logger:
-    return logging.getLogger(name)
+
+# def LOGGER(name: str) -> logging.Logger:
+#     return logging.getLogger(name)
