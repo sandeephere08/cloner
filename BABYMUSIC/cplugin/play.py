7e9510638b8514baa331087aad34faa176d56fd9
Yasir Akhtar <yasiralam662@gmail.com>
2025-01-03 02:58:46 +0530

    New CLone !

--- BABYMUSIC/cplugin/play.py
+++ BABYMUSIC/cplugin/play.py
@@ -785,7 +785,7 @@ from BABYMUSIC.utils.inline import (
     stream_markup2,
     panel_markup_4,
 )
-from BABYMUSIC.utils.pastebin import BABYBin
+from BABYMUSIC.utils.pastebin import PROBin
 from BABYMUSIC.utils.stream.queue import put_queue, put_queue_index
 from youtubesearchpython.__future__ import VideosSearch
 
@@ -894,7 +894,7 @@ async def stream(
         if count == 0:
             return
         else:
-            link = await BABYBin(msg)
+            link = await PROBin(msg)
             lines = msg.count("\n")
             if lines >= 17:
                 car = os.linesep.join(msg.split(os.linesep)[:17])
