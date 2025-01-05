eb65edf60fed80a4b89458e8b212c61349294096
Moon <101085046+MOON-CODES@users.noreply.github.com>
2025-01-05 09:07:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -54,7 +54,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/MOON-CODES/Xd/blob",
+    "https://github.com/MOON-CODES/Xd",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
