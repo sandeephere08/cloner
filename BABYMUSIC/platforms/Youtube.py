78dacb2f40ba63f4b835c862498c78fff77ee3a0
Moon <101085046+MOON-CODES@users.noreply.github.com>
2025-01-07 14:11:51 +0530

    Moon 🌙

--- BABYMUSIC/platforms/Youtube.py
+++ BABYMUSIC/platforms/Youtube.py
@@ -9,8 +9,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from AviaxMusic.utils.database import is_on_off
-from AviaxMusic.utils.formatters import time_to_seconds
+from BABYMUSIC.utils.database import is_on_off
+from BABYMUSIC.utils.formatters import time_to_seconds
 
 
 
