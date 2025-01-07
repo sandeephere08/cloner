23f036a3e4de993d17a30149d804c16c458f56b6
Moon <101085046+MOON-CODES@users.noreply.github.com>
2025-01-07 06:25:20 +0530

    Update help.py

--- BABYMUSIC/cplugin/help.py
+++ BABYMUSIC/cplugin/help.py
@@ -95,7 +95,7 @@ async def helper_cb(client, CallbackQuery, _):
     if cb == "hb9":
         if CallbackQuery.from_user.id not in SUDOERS:
             return await CallbackQuery.answer(
-                "😎 Pehle Zeo Ko Papa Bol Jake 😆😆", show_alert=True
+                "😎 Hɪ ɪᴀᴍ ᴜɴᴛᴏʟᴅ ᴄᴏᴅᴇʀ  😆😆", show_alert=True
             )
         else:
             await CallbackQuery.edit_message_text(helpers.HELP_9, reply_markup=keyboard)
