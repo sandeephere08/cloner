53fcbd6cd6a0c6152351bc591510ec5fb74cd7dd
Moon <101085046+MOON-CODES@users.noreply.github.com>
2025-01-07 15:39:15 +0530

    Moon 🌙

--- BABYMUSIC/utils/inline/queue.py
+++ BABYMUSIC/utils/inline/queue.py
@@ -1,6 +1,5 @@
 from typing import Union
-from BABYMUSIC import app
-from BABYMUSIC.utils.formatters import time_to_seconds
+
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
@@ -67,11 +66,27 @@ def queue_back_markup(_, CPLAY):
 def aq_markup(_, chat_id):
     buttons = [
         [
-            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
-            InlineKeyboardButton(text="skip", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="🔸ᴘᴀᴜsᴇ🔸", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="▫️sᴛᴏᴘ▫️", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="▪️sᴋɪᴘ▪️", callback_data=f"ADMIN Skip|{chat_id}"),
+        ],
+        [
+            InlineKeyboardButton(
+                text="🔸ʀᴇsᴜᴍᴇ🔸", callback_data=f"ADMIN Resume|{chat_id}"
+            ),
+            InlineKeyboardButton(
+                text="▪️ʀᴇᴘʟᴀʏ▪️", callback_data=f"ADMIN Replay|{chat_id}"
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="▫️ᴍᴏʀᴇ▫️",
+                url="https://t.me/Inflexupdate",
+            ),
         ],
     ]
     return buttons
+
 
 def queuemarkup(_, vidid, chat_id):
 
@@ -84,24 +99,24 @@ def queuemarkup(_, vidid, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text="ᴘᴀᴜsᴇ",
+                text="▪️ᴘᴀᴜsᴇ▪️",
                 callback_data=f"ADMIN Pause|{chat_id}",
             ),
-            InlineKeyboardButton(text="sᴛᴏᴘ", callback_data=f"ADMIN Stop|{chat_id}"),
-            InlineKeyboardButton(text="sᴋɪᴘ", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="🔸sᴛᴏᴘ🔸", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="▫️sᴋɪᴘ▫️", callback_data=f"ADMIN Skip|{chat_id}"),
         ],
         [
             InlineKeyboardButton(
-                text="ʀᴇsᴜᴍ", callback_data=f"ADMIN Resume|{chat_id}"
+                text="▪️ʀᴇsᴜᴍᴇ▪️", callback_data=f"ADMIN Resume|{chat_id}"
             ),
             InlineKeyboardButton(
-                text="ʀᴇᴘʟᴀ", callback_data=f"ADMIN Replay|{chat_id}"
+                text="▫️ʀᴇᴘʟᴀʏ▫️", callback_data=f"ADMIN Replay|{chat_id}"
             ),
         ],
         [
             InlineKeyboardButton(
-                text="๏ ᴍᴏʀᴇ ๏",
-                url="https://t.me/ProBotts",
+                text="▪️ ᴍᴏʀᴇ ▪️",
+                url="https://t.me/Inflexupdate",
             ),
         ],
     ]
